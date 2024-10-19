d960ddea2acb2a2489fe359ce3724c4347c8f58a
EmAyala6 <emily.ayala625@gmail.com>
2024-10-18 18:29:40 -0700

    -Fixed Cart pushing, removed Barrel Scipt and replaced it with PickableItems Script, Fixed Pickup, Added DropItem mechanic, Added holdPoint to characters hands to hold items

--- Assets/Scripts/Cart.cs
+++ Assets/Scripts/Cart.cs
@@ -15,12 +15,19 @@ public class Cart : MonoBehaviour, IInteractable
         rb = GetComponent<Rigidbody>();
         renderer = GetComponent<Renderer>();
         originalMaterial = renderer.material;
+        //constraint to prevent undesired movement
+        rb.constraints= RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
     }
 
     public void Interact()
     {
         isBeingPushed = !isBeingPushed;
         Debug.Log("Cart interaction toggled. Is being pushed: " + isBeingPushed);
+
+        if (!isBeingPushed)
+        {
+            rb.velocity = Vector3.zero; //stops cart immediately
+        }
     }
 
     void FixedUpdate()
@@ -28,19 +35,12 @@ public class Cart : MonoBehaviour, IInteractable
         if (isBeingPushed)
         {
             // Apply force to the cart to push it forward
-            rb.AddForce(transform.forward * 10f);
+            rb.AddForce(transform.forward * 2f);
         }
     }
 
     public void Highlight(bool highlight)
     {
-        if (highlight)
-        {
-            renderer.material = highlightMaterial;
-        }
-        else
-        {
-            renderer.material = originalMaterial;
-        }
+       renderer.material = highlight ? highlightMaterial : originalMaterial;
     }
 }
--- Assets/Scripts/InteractionPrompt.cs
+++ Assets/Scripts/InteractionPrompt.cs
@@ -5,34 +5,43 @@ using UnityEngine.UI;
 
 public class InteractionPrompt : MonoBehaviour
 {
+
     public Text promptText;
     public float interactionDistance = 3f;
     public LayerMask interactableLayer;
 
     void Update()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, interactionDistance, interactableLayer))
         {
-            IInteractable interactable = hit.collider.GetComponent<IInteractable>();
-            if (interactable != null)
+            if (promptText == null)
             {
-                promptText.gameObject.SetActive(true);
-                if (Input.GetKeyDown(KeyCode.E))
+                Debug.LogError("Prompt Text is not assigned!");
+                return;
+            }
+                Ray ray = new Ray(transform.position, transform.forward);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, interactionDistance, interactableLayer))
                 {
-                    interactable.Interact();
+                    IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+                    if (interactable != null)
+                    {
+                        promptText.gameObject.SetActive(true);
+
+                        if (Input.GetButtonDown("Interact"))
+                        {
+                            interactable.Interact();
+                        }
+                    }
+                    else
+                    {
+                        promptText.gameObject.SetActive(false);
+                    }
+                }
+                else
+                {
+                    promptText.gameObject.SetActive(false);
                 }
             }
-            else
-            {
-                promptText.gameObject.SetActive(false);
-            }
-        }
-        else
-        {
-            promptText.gameObject.SetActive(false);
         }
     }
-}
